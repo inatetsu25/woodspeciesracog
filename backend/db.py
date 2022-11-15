3d0cca00419a5f281dfd7ffc44b41a08f8e06c84
kimura572 <kimurafumiya7020@icloud.com>
2022-11-15 22:16:17 +0900

    データベースid追加

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,7 @@ import pandas as pd
 def select_data():
     con = sqlite3.connect('./sample.db')
     cur = con.cursor()
-    # cur.execute("SELECT * FROM TIME_SPECIES")
+    cur.execute("CREATE TABLE IF NOT EXISTS TIME_SPECIES(id INTEGER PRIMARY KEY AUTOINCREMENT, time DATETIME,true_name TEXT, predict_name TEXT, TF TEXT)")
     df = pd.read_sql('SELECT * FROM TIME_SPECIES', con)
     cur.close()
     con.close()
@@ -13,8 +13,9 @@ def select_data():
 
 def insert_data(time,true_name,predict_name):
     con = sqlite3.connect('./sample.db')
+    sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS TIME_SPECIES(time datetime,true_name text, predict_name text, TF text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS TIME_SPECIES(id INTEGER PRIMARY KEY AUTOINCREMENT, time DATETIME,true_name TEXT, predict_name TEXT, TF TEXT)")
     sql = 'INSERT INTO TIME_SPECIES (time, true_name, predict_name, TF) values (?,?,?,?)'
 
     if true_name=='nodata':
