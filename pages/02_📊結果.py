af06fd06bc5acee01285c6579309d2a7949c3630
kimura572 <kimurafumiya7020@icloud.com>
2022-11-18 23:27:33 +0900

    データベース削除機能id基準

--- pages/02_📊結果.py
+++ pages/02_📊結果.py
@@ -11,6 +11,10 @@ st.set_page_config(
 st.title('木検索アプリ')
 st.header('結果一覧')
 
+delete_id = st.text_input('idを入力')
+
+st.button(label='削除する')
+db.delete(delete_id)
+
 db_df = db.select_data()
 st.write(db_df)
-db.delete()
