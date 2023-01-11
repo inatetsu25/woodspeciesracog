f225c33715fbbe800063261002f2f36d4da4329d
kimura572 <kimurafumiya7020@icloud.com>
2023-01-11 15:04:38 +0900

    エラー対応

--- 01_home.py
+++ 01_home.py
@@ -21,10 +21,9 @@ import streamlit as st
 from backend import predict, preprocess, csv_function
 
 
-REFRESH_TOKEN = st.secrets['REFRESH_TOKEN']
-APP_KEY = st.secrets['APP_KEY']
-APP_SECRET = st.secrets['APP_SECRET']
-
+REFRESH_TOKEN = st.secrets["REFRESH_TOKEN"]
+APP_KEY = st.secrets["APP_KEY"]
+APP_SECRET = st.secrets["APP_SECRET"]
 
 file_path = "result.csv"
 dbx_path = "/result.csv"
